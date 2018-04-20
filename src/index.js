68b941e35ff62876f3f0a908903d99ec270c8b66
pranojka <jovana.pranic@gmail.com>
2018-04-19 19:09:20 -0700

    testiranje

--- src/index.js
+++ src/index.js
@@ -1,7 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+const Template =() => (
+    <div>
+        
+    <h1>Cao, drugari :)</h1>
+    <p>Ovo je samo proba</p>
+    </div>
+);
 
 ReactDOM.render(
-	<h1>Cao, drugari :)</h1>,document.getElementById('root')
-	);
+	<Template />,
+    document.getElementById('root')
+);
