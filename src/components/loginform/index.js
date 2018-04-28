50276a237e3c533cd4fb5613e5ce135d413ccadd
anticni <n.antic91@gmail.com>
2018-04-28 16:21:26 +0200

    copied loginform from branch jovanap

--- src/components/loginform/index.js
+++ src/components/loginform/index.js
@@ -1,13 +1,78 @@
-import React from 'react';  
+import React from 'react';
+import { Redirect, withRouter } from "react-router-dom";  
 import './style.css'  
  
 class LoginForm extends React.Component { 
   constructor(props){ 
     super(props) 
-  } 
+    this.state={
+        email: "",
+        password: "",
+        isValid: false,
+    }
+}
+
+logIn(e) {
  
-  render(){ 
-    return <p>LoginForm</p>; 
+    event.preventDefault();
+    const handleLogin=this.props.handleLogin; 
+    console.log("this.state", this.state);
+    const{email,password}=this.state;
+    
+
+
+    
+
+    if (!(email===""&& password==="")){
+        this.setState({isValid: true})
+
+
+
+        return;
+
+    handleLogin();
+
+  } }
+ 
+  render() { 
+
+    
+    const {isValid}=this.state;
+
+    if(this.state.isValid){
+
+        return <Redirect to={"/home"}/>;
+    }
+
+    return ( 
+       <div className="Form">
+
+       <h2>LOG IN </h2>
+        
+
+        <input 
+        type= "email" required=""
+        name="email"
+        placeholder ="Email"
+        onChange={e=>this.setState({email : event.target.value})}/>
+
+       <input 
+        name= "password"
+        placeholder ="Password"
+        type="password" required=""
+        onChange={e=>this.setState({password : event.target.value})}/>
+
+        <button 
+        type="submit"
+        onClick={()=>this.logIn()}>LOG IN </button>
+
+
+
+
+
+  
+
+    </div>); 
   } 
 } 
  
