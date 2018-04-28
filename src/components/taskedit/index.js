844b35ca3e3feac32969a8524467ab7957d1c981
anticni <n.antic91@gmail.com>
2018-04-28 16:52:22 +0200

    copied taskedit from branch jovanap

--- src/components/taskedit/index.js
+++ src/components/taskedit/index.js
@@ -1,13 +1,81 @@
 import React from 'react';  
-import './style.css'  
+import './style.css';
+import Modal from "react-modal";
+
+
+const customStyles = {
+  content : {
+    top                   : '50%',
+    left                  : '50%',
+    right                 : 'auto',
+    bottom                : 'auto',
+    marginRight           : '-50%',
+    transform             : 'translate(-50%, -50%)'
+  }
+};
  
 class TaskEdit extends React.Component { 
   constructor(props){ 
-    super(props) 
-  } 
+    super(props)
+
+     
+    this.state = {
+        tasks:[],
+      modalIsOpen: false
+    };
  
+    this.openModal = this.openModal.bind(this);
+    this.afterOpenModal = this.afterOpenModal.bind(this);
+    this.closeModal = this.closeModal.bind(this);
+  }
+ 
+  openModal() {
+    this.setState({modalIsOpen: true});
+  }
+ 
+  afterOpenModal() {
+    // references are now sync'd and can be accessed.
+    this.subtitle.style.color = '#f00';
+  }
+ 
+  closeModal() {
+    this.setState({modalIsOpen: false});
+  }
+
+
+
+ 
+ 
+
+
+
+
   render(){ 
-    return <p>TaskEdit</p>; 
+
+    return ( <div>
+
+    <button onClick={this.openModal}>Open Modal</button>
+        <Modal
+          isOpen={this.state.modalIsOpen}
+          onAfterOpen={this.afterOpenModal}
+          onRequestClose={this.closeModal}
+          style={customStyles}
+          contentLabel="Example Modal"
+        >
+ 
+          <h2 ref={subtitle => this.subtitle = subtitle}>Hello</h2>
+          <button onClick={this.closeModal}>close</button>
+          <div>EDIT</div>
+          <form>
+            <input type="text" value={this.props.description}/>
+            <input />
+            <button>save</button>
+
+
+          </form>
+        </Modal>
+
+        </div> );
   } 
 } 
  
