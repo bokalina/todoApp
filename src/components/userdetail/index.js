b1fa38e87af2b4258d73c029d1bd44ceb5d724fa
Pranic <jp250398@ncr.com>
2018-05-04 17:33:46 +0200

    addeduserlist

--- src/components/userdetail/index.js
+++ src/components/userdetail/index.js
@@ -1,14 +1,71 @@
-import React from 'react';  
-import './style.css'  
+import React from 'react'; 
+import './style.css' ;
  
 class UserDetail extends React.Component { 
-  constructor(props){ 
-    super(props) 
-  } 
- 
-  render(){ 
-    return <p>UserDetail</p>; 
-  } 
-} 
- 
-export default UserDetail
+ constructor(props){ 
+  super(props);
+  this.getUsers=this.getUsers.bind(this);
+  // this.getOne=this.getOne.bind(this);
+  this.state = {
+    users: [],
+    user: {},
+  } } 
+
+
+// getOne(username){
+//   this.setState( prevState => ({
+//  user: prevState.users.filter(users => users.username == username) } )
+// }
+
+  getUsers(){
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(response => response.json())
+    .then(json => this.setState(() => ({
+   users: json
+  })))
+ }
+
+
+ componentDidMount(){
+  console.log('componentdidmount')
+  this.getUsers();
+ }
+
+ render(){
+  return(
+    <table>
+      <thead>
+     <tr>
+      <th>ID</th>
+      <th>name</th>
+      <th>username</th>
+      <th>email</th>
+     </tr>
+    </thead>
+    <tbody>
+     {
+      this.state.users.map((user) => (
+       <User
+        key={user.id}
+        id={user.id}
+        name={user.name} 
+        username={user.username} 
+        email={user.email}
+       />
+      ))
+     }
+    </tbody>
+    </table>
+    );
+ }
+}
+const User = (props) => (
+ <tr>
+  <td>{props.id}</td>
+  <td>{props.name}</td>
+  <td>{props.username}</td>
+  <td>{props.email}</td>
+ </tr>
+);
+
+export default UserDetail;
