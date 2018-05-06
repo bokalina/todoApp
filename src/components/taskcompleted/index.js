5ce9ba86569695852855675dd67ca19aee134a7b
anticni <n.antic91@gmail.com>
2018-05-06 22:47:43 +0200

    bugfixes, added fetch for serverside change update

--- src/components/taskcompleted/index.js
+++ src/components/taskcompleted/index.js
@@ -4,16 +4,37 @@ import './style.css'
 class TaskCompleted extends React.Component { 
   constructor(props){ 
     super(props);
+    this.state = {
+        checked : this.props.done
+    }
+
     this.onChange = this.onChange.bind(this); 
   } 
 
-  onChange(){
-    console.log('checkbox clicked');
+  onChange(event){
+    console.log("onChange@taskcompleted");
+    
+    const data = new FormData();
+    data.append('done', String(event.target.checked));
+    const url = `https://anticni.pythonanywhere.com/api/tasks/isdone/${this.props.id}`;
+
+    fetch(url, {
+              method: 'POST',
+              body: data
+        }).then(function(response) {
+        console.log("ok");
+    }).catch(function(error) {
+        alert("Server Error\nPlease Try Again");
+        return;
+    });
+
     this.props.checkbox(this.props.id);
+
+
   }
  
   render(){ 
-    return <input id="checkBox" type="checkbox" checked={this.props.done} onChange={this.onChange} />
+    return <input id="checkBox" type="checkbox" checked={this.state.checked} onChange={this.onChange} />
   } 
 } 
  
